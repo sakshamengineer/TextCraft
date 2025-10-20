f53bf70b2d32f198ce71f27edf6a80a103357817
sakshamengineer <sakshamkgupta01@gmail.com>
2025-10-20 08:03:37 +0530

    bug fixed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,14 +5,14 @@ function Navbar(props) {
         <div>
             <nav className={`navbar navbar-expand-lg bg-${props.toggle} navbar-${props.toggle}`}>
                 <div className="container-fluid">
-                    <Link className="mx-3 navbar-brand" to='/reactapp'>{props.title}</Link>
+                    <Link className="mx-3 navbar-brand" to='/TextCraft'>{props.title}</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to='/reactapp'>Home</Link>
+                                <Link className="nav-link active" aria-current="page" to='/TextCraft'>Home</Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link" to='/about'>{props.abtxt}</Link>
